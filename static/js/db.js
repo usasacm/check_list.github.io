5c5974a3d79fd7d181fe39724505d162eaf227b1
usasacm <usasacm@gmail.com>
2020-11-05 12:57:51 +0700

    change checklist item value

--- static/js/db.js
+++ static/js/db.js
@@ -1,7 +1,7 @@
 /*var menu_data = getData('/menu_json')
 var mkb_data = getData('/mkb');*/
 /*--prod db version 13--*/
-const dbVersion = 15;
+const dbVersion = 16;
 
 if(!window.indexedDB){
   console.log("not support");
