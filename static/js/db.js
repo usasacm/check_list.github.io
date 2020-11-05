5dffe0fd848dd766bb84bbf135f2f56f652e6059
usasacm <usasacm@gmail.com>
2020-11-05 10:47:06 +0700

    add new check-list

--- static/js/db.js
+++ static/js/db.js
@@ -1,7 +1,7 @@
 /*var menu_data = getData('/menu_json')
 var mkb_data = getData('/mkb');*/
 /*--prod db version 13--*/
-const dbVersion = 13;
+const dbVersion = 15;
 
 if(!window.indexedDB){
   console.log("not support");
